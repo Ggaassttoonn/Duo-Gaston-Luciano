19cc431c173d8856d60a9c9c88e02ea8e343153d
Gastón Azula <azulagaston250@gmail.com>
2026-05-21 20:34:46 -0300

    Agregar migracion de tabla users

---
+++ database/migrations/2026_05_21_062505_create_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            // Esto crea la columna 'persona_id' y la vincula a tu tabla 'personas'
+            $table->foreignId('persona_id')->constrained('personas')->onDelete('cascade');
+            
+            $table->string('name')->unique();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
